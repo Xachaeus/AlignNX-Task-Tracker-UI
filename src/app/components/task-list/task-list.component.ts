c83c8f6ea058390070b81cca7d0a63b771c4a04e
Zack Sollenberger <zack@alignnx.com>
2026-06-09 11:49:55 -0400

    Created dropdown functionality for the 'My Tasks' list and added progress bar

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, signal, SimpleChanges } from '@angular/core';
 import { TaskItemComponent } from '../task-item/task-item.component';
-import { Goal, Task } from '../../models/task.model';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-list',
@@ -10,7 +10,25 @@ import { Goal, Task } from '../../models/task.model';
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent  {
+export class TaskListComponent {
   @Input() tasks!: Task[];
   @Output() toggleTask = new EventEmitter<{task_id: string, parent_id: string}>();
+
+  expanded = signal<boolean>(true);
+  isComplete = signal<boolean>(false);
+  progress = signal<number>(0.0);
+
+
+  TaskUpdateCallback(event: {task_id: string, parent_id: string}, taskChangedState: boolean) {
+    this.toggleTask.emit(event);
+    if (this.tasks.length === 0) {this.progress.set(0.0);}
+    else {this.progress.set(Math.round(((this.tasks.filter(t => t.completed).length + (taskChangedState ? -1 : 1)) / this.tasks.length) * 100));}
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['tasks']) {
+      if (this.tasks.length === 0) {this.progress.set(0.0);}
+      else {this.progress.set(Math.round((this.tasks.filter(t => t.completed).length / this.tasks.length) * 100));}
+    }
+  }
 }
